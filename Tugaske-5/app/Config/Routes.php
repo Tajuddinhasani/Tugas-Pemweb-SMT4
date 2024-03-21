2ae60c7c5767fed8cecb215a5940d36b576b5cc1
Tajuddin Hasani Zamzami <145316067+Tajuddinhasani@users.noreply.github.com>
2024-03-21 22:25:31 +0700

    pertemuan 5

---
+++ Tugaske-5/app/Config/Routes.php
@@ -0,0 +1,12 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+$routes->get('/home', 'Pages::home');
+$routes->get('/about', 'Pages::about');
+$routes->get('/kontak', 'Pages::kontak');
+$routes->setAutoRoute(true);
---
+++ Tugaske-5/app/Controllers/Pages.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controllers;
+
+class Pages extends BaseController
+{
+    public function home()
+    {
+        $data = [
+            'title' => 'Home | Unipdu Press',
+            'tes' => ['satu', 'dua', 'tiga']
+        ];
+        echo view('layout/header', $data);
+        echo view('pages/home');
+        echo view('layout/footer');
+    }
+    public function about()
+    {
+        $data = [
+            'title' => 'Tentang | Unipdu Press',
+            'tes' => ['satu', 'dua', 'tiga']
+        ];
+        echo view('layout/header', $data);
+        echo view('pages/about');
+        echo view('layout/footer');
+    }
+    public function kontak()
+{
+    $data = [
+        'title' => 'Tentang | Unipdu Press',
+        'alamat' => [
+            ['tipe' => 'alamat rumah', 'alamat' => 'kanjar pangilen', 'kota' => 'Sampang']
+        ],
+        'kantor' => [
+            ['tipe' => 'kantor', 'tempat' => 'Pabrik Garam Madura', 'kota' => 'Sampang']
+        ]
+    ];
+    echo view('layout/header', $data);
+    echo view('pages/kontak', $data); // Mengirimkan $data ke tampilan kontak.php
+    echo view('layout/footer');
+}
+
+}
